07a87256ea1b376b9cc7f1ffac24e4f55946cd89
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-07 22:17:21 +0400

    Initial commit

---
+++ Simpletests.py
@@ -0,0 +1,63 @@
+# TEST 2
+# a = int(input("Eded daxil edin "))
+# print(len(str(a))
+
+# ProjectEuler.net
+# b  = 2 ** 1000
+# count = 0
+# while b > 1:
+#     a = b%10
+#     count += a
+#     b = b // 10
+# print(count)
+
+
+# 903
+# a =abs(int(input()))
+
+# b = a % 10
+# c = (a // 10) % 10
+# d = a // 100
+#
+# if b == d:
+#     print("=")
+# else:
+#     print(max(b,d))
+
+# 905
+# a,b,c = map(int, input().split())
+# if a == b == c:
+#     print("1")
+# elif a == b or a == c or b == c:
+#     print("2")
+# else:
+#     print("3")
+
+# 906
+# a = int(input())
+# b = a % 10
+# c = (a // 10) % 10
+# d = a // 100
+# product = b * c * d
+# print(product)
+
+# 909
+# a = str(input())
+# res = len(a.split())
+# print(str(res))
+
+# 915
+# a,b,c = map(int,input().split())
+# if a**2 + c**2 == b**2 or a**2 + b**2  == c**2 or b**2 +c**2 == a**2:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+
+
+
+
+
+
+
