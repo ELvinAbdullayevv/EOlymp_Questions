d20000cfe13c0cd71b37b91f617d4168678c2689
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-09 20:41:40 +0400

    codes updated

---
+++ baku.py
@@ -0,0 +1,79 @@
+# 1000
+# import sys
+#
+# for s in sys.stdin:
+#
+#   a, b = map(int,s.split())
+#
+#   print(a+b)
+
+# 8806
+# a, b = map(int, input().split())
+# print(a+b)
+
+
+# 8809
+# a,b = map(int,input().split())
+# print(a-b)
+
+# 8808
+# a = int(input())
+# b = int(input())
+# print(a-b)
+
+# 8810
+# a,b,c = map(int, input().split())
+# print(a+b-c)
+
+# 8811
+# a,b =  map(int, input().split())
+# print(a*b)
+
+# 8812
+# a, b = map(int, input().split())
+# p = 2*(a+b)
+# s = a *b
+# print(p,s)
+
+# 8814
+# a = int(input())
+# p = 4 * a
+# s = pow(a,2)
+# print(p,s)
+
+# 8817
+# n = int(input())
+# # count = 20 * (21 ** (n-1))
+# # print(count)
+
+# 9575
+# a = float(input())
+# x = "{:.4f}".format(3.14 * a ** 2)
+# print(x)
+
+# 9576
+# a = float(input())
+# x = "{:.3f}".format(3.14 * a * 2)
+# print(x)
+
+# 9900
+# n = int(input())
+# print(n)
+
+# 9901
+# n = int(input())
+# print(n-1)
+
+# 9902
+# n = int(input())
+# c = n // 2
+# print(c)
+
+# 9903
+# n = int(input())
+# if n % 2 == 0:
+#     print(n // 2)
+# else:
+#     if n % 2 != 0:
+#         print((n // 2)+1)
+
