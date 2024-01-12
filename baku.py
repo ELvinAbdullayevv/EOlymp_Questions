39da65f2070d906c86eaa4d89ff0b9bccad664fb
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-12 21:03:22 +0400

    codes updqted

--- baku.py
+++ baku.py
@@ -156,5 +156,23 @@
 
 
 
+# 1605
+# number = int(input("Enter a number: "))
+#
+# if number < 2:
+#     print(f"{number} is not a prime number.")
+# else:
+#     is_prime = True
+#     for i in range(2, int(number**0.5) + 1):
+#         if number % i == 0:
+#             is_prime = False
+#             break
+#
+#     if is_prime:
+#         print(f"{number} is a prime number.")
+#     else:
+#         print(f"{number} is not a prime number.")
+
+
 
 
