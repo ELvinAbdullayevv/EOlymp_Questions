344db41fd952d60c5531de709656302bb667dde6
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-15 21:03:43 +0400

    codes updated

---
+++ Task.py
@@ -0,0 +1,31 @@
+# TASK 1
+# a = list(input().split())
+
+# a.sort(key = len)
+
+
+# for word in a:
+#     print(word)
+
+
+# Task 3
+# numbers = list(map(float, input("ededi daxil edin ").split()))
+
+# product = 1
+
+# for element in numbers:
+#     product *= element
+
+# print(product)
+
+
+# Task 4
+# a = list(map(int, input("Ededi daxil edin ").split()))
+
+# b = list(map(int,input("Ededi daxil edin ").split()))
+
+# for i in b:
+#     a.append(i)
+#     a.sort()
+
+# print(a)
