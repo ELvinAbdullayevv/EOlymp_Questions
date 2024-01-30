c1e5f1633278a2ab9928679c24b3b5f0dd665cee
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-30 05:28:50 +0400

    codes updated

--- Task.py
+++ Task.py
@@ -33,24 +33,14 @@
 
 # EBOB TASK
 
-def find_deviders(num):
-    result = set()
-    result.add(num)
-    max_num = int(num / 2) + 1
-    for i in range(1, max_num):
-        if num % i == 0:
-            result.add(i)
+def abc():
+    b = input("eded daxil edin")
 
-    return result
+    try:
+        b = float(b)
+        print("{}".format(round(b)))
+    except:
+        print("{} bu variable errordur".format(b))
 
 
-num1 = int(input("first number:"))
-num2 = int(input("second number:"))
-
-first_devidors = find_deviders(num1)
-second_devidors = find_deviders(num2)
-intersection = first_devidors & second_devidors
-print(num1, "sayisinin bolenleri", first_devidors)
-print(num2, "sayisinin bolenleri", second_devidors)
-print("kesisen bolenleri", intersection)
-print("EBOB: ", max(intersection))
+print(abc())
