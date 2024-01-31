c467caa50f87cf6ac1e9ecfa22ebdb0387e6db71
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-02-01 00:37:09 +0400

    codes updated

--- Task.py
+++ Task.py
@@ -46,16 +46,49 @@
 # print(abc())
 
 
-luget = {
-    'meyve' : 'fruit' ,
-    'kitab' : 'book' ,
-    'komputer' : 'computer' ,
-    'masa' : 'desk' ,
-    'qelem' : 'pen' ,
-}
-deleted_word = input()
+# luget = {
+#     'meyve' : 'fruit' ,
+#     'kitab' : 'book' ,
+#     'komputer' : 'computer' ,
+#     'masa' : 'desk' ,
+#     'qelem' : 'pen' ,
+# }
+# deleted_word = input()
+#
+#
+# if deleted_word in luget:
+#         del luget[deleted_word]
+# print(luget)
 
 
-if deleted_word in luget:
-        del luget[deleted_word]
-print(luget)
+# Task1
+
+# book = {"writer" : "Nizami Gencevi",
+#         "poem" : "Yeddi Gozel",
+#         "year" : 1147}
+
+
+# another_book = {"writer" : "Nizami Gencevi",
+#                 "poem" : "Leyli Mecnun",
+#                 "year" : 1188}
+
+
+# same_values_and_keys = {}
+
+# for i in set(book) and  set(another_book):
+#     if book[i] == another_book[i]:
+#         same_values_and_keys[i] = (book[i])
+
+# print(same_values_and_keys)
+
+
+# Task3
+# dict1 = {12, 13, 14, 24, 9, 27, 51}
+
+
+# dict2= []
+
+# for i in dict1:
+#     if i % 3 == 0:
+#         dict2.append(i)
+# print(sorted(dict2))
