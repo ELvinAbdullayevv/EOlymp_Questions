988409b8365d51da4bee8a332ee484e23a089fc1
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-02-11 14:27:16 +0400

    codes updated

--- Task.py
+++ Task.py
@@ -132,7 +132,7 @@
 
 
 
-Task1
+# Task1
 number1 = input()
 number2 = input()
 calculation = input()
@@ -181,3 +181,33 @@ match calculation:
         division(number1, number2)
     case "*":
         multiply(number1, number2)
+
+
+
+
+
+# Task2
+import sys
+
+inputs = []
+if len(sys.argv) > 1:
+    a = sys.argv[1]
+    if a == "add":
+        title1 = input("Enter Series Name: ")
+        title2 = input("Enter director name: ")
+        title3 = input("Enter series of episodes: ")
+        title4 = input("Enter genre: ")
+        b = [title1, title2, title3, title4]
+        inputs.append(b)
+        print(*inputs)
+
+
+    else:
+        print("please write add")
+
+try:
+    with open("C:/Users/user/Desktop/python/newtext.txt", "a") as file:
+        for item in inputs:
+            file.write(str(item) + "\n")
+except Exception:
+    print("An error occurred while writing to the file:", )
